b82c186a48b9a2881a3774ac1dc6e2d47b73b67b
anjalijv <anjali.jammulamadaka@mail.utoronto.ca>
2026-04-04 19:26:59 -0400

    LoginTest

--- backend/src/test/java/com/group9/postal/LoginTest.java
+++ backend/src/test/java/com/group9/postal/LoginTest.java
@@ -1,4 +1,41 @@
 package com.group9.postal;
 
+import com.group9.postal.repository.UserRepository;
+import com.group9.postal.model.User;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+@AutoConfigureMockMvc
 public class LoginTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Test
+    void customerLoginSuccess() throws Exception {
+        ObjectNode loginJson = objectMapper.createObjectNode();
+
+    }
+
 }
